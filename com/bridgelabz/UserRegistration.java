205b6c537b4db6c1d5414cda3366c9afa7c97687
pranay <pranaykawale500@gmail.com>
2021-11-21 17:42:08 +0530

    Added:Welcome message added

---
+++ com/bridgelabz/UserRegistration.java
@@ -0,0 +1,27 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class UserRegistration {
+	Scanner sc = new Scanner(System.in);
+	public void checkFirstName(){
+		System.out.print("Enter First Name of User: " );
+		String  fName = sc.next();
+		boolean check = Pattern.matches("^[A-Z]{1}+[a-z]{3,}$", fName);
+		if (check)
+			System.out.println("Perfect! First #Name");
+		else
+			System.out.println("Please Enter a valid First Name");
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		UserRegistration user = new UserRegistration();
+
+		System.out.println("Welcome to User Registration Program");
+
+		user.checkFirstName();
+
+	}
+}
+
