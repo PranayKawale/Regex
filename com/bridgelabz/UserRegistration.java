df4baa1f36d339bfde5ec394ec6f17793c3c8d33
pranay <pranaykawale500@gmail.com>
2021-11-24 21:29:57 +0530

    Added:Following predefined Mobile format

--- com/bridgelabz/UserRegistration.java
+++ com/bridgelabz/UserRegistration.java
@@ -35,6 +35,16 @@ public class UserRegistration {
         else
             System.out.println("Please Enter a Valid Email with Only \"example.abc@bl.co.in\" latter");
     }
+    public void validMobileNum(){
+       
+        System.out.print("Enter Mobile number with country code (91)  : " );
+        String mobileNum = sc.nextLine();
+        boolean check = Pattern.matches("^91\\s[6789][0-9]{9}", mobileNum);
+        if (check)
+            System.out.println("Perfect! Mobile #Num");
+        else
+            System.out.println("Please Enter a Valid Mobile Num ex:- 90 9533182605");
+    }
 	public static void main(String[] args) {
 		UserRegistration user = new UserRegistration();
 
@@ -43,6 +53,7 @@ public class UserRegistration {
 		user.checkFirstName();
 		user.checkLastName();
 		user.enterValidEmail();
+		user.validMobileNum();
 	}
 }
 
