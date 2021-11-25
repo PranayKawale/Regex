8e6dac29249fca7e25fae30bad118db8700bdd2e
pranay <pranaykawale500@gmail.com>
2021-11-25 05:53:53 +0530

    Added:Password should have at least upper case

--- com/bridgelabz/UserRegistration.java
+++ com/bridgelabz/UserRegistration.java
@@ -55,6 +55,16 @@ public class UserRegistration {
         else
             System.out.println("Please Enter a Valid password, it should have minimum 8 characters");
     }
+    public void checkPassWordRule2(){
+        
+        System.out.print("Enter password :  ");
+        String passWord = sc.nextLine();
+        boolean check = Pattern.matches("[A-Z]{1}[a-z]{7,}", passWord);
+        if (check)
+            System.out.println("Perfect! #password");
+        else
+            System.out.println("Please Enter a Valid password, it should have minimum 8 characters with 1 upper case");
+    }
 	public static void main(String[] args) {
 		UserRegistration user = new UserRegistration();
 
@@ -65,6 +75,7 @@ public class UserRegistration {
 		user.enterValidEmail();
 		user.validMobileNum();
 		user.checkPassWordRule1();
+		user.checkPassWordRule2();
 	}
 }
 
